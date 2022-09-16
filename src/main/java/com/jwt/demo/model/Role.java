c67393dc477cb7ac45ef2cba00e1754439a6bcf3
Fatih <curaat79@gmail.com>
2022-09-16 19:27:14 +0300

    Add Model Classes

---
+++ src/main/java/com/jwt/demo/model/Role.java
@@ -0,0 +1,15 @@
+package com.jwt.demo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+
+    ROLE_ADMIN, ROLE_USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+
+
+}
---
+++ src/main/java/com/jwt/demo/model/User.java
@@ -0,0 +1,25 @@
+package com.jwt.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Document
+public class User {
+
+    @Id
+    private Integer id;
+
+    @Size(min = 4, max = 16, message = "Username length must between 4 and 16 char")
+    private String username;
+
+    private String email;
+
+    @Size(min = 4, message = "Password length must at least 4 char")
+    private String password;
+
+    private List<Role> userRoles;
+
+}
