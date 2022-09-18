abe525d0afe88474721532730b42d8c8c03c52d1
Fatih <curaat79@gmail.com>
2022-09-18 13:51:20 +0300

    Add UserService for Business Logic

--- src/main/java/com/jwt/demo/security/SecurityConfig.java
+++ src/main/java/com/jwt/demo/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.jwt.demo.security;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -34,5 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
 }
--- src/main/java/com/jwt/demo/service/UserService.java
+++ src/main/java/com/jwt/demo/service/UserService.java
@@ -1,17 +1,79 @@
 package com.jwt.demo.service;
 
+import com.jwt.demo.exception.CustomException;
+import com.jwt.demo.model.User;
 import com.jwt.demo.repository.UserRepository;
+import com.jwt.demo.security.JwtProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+    private final AuthenticationManager authenticationManager;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtProvider jwtProvider, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtProvider = jwtProvider;
+        this.authenticationManager = authenticationManager;
     }
 
-    
+    public String signIn(String username, String password){
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
+
+            Optional<User> optUser = userRepository.findByUsername(username);
+            User user = optUser.orElseThrow(() -> new CustomException("ss", HttpStatus.BAD_REQUEST));
+
+            return jwtProvider.getToken(username, user.getUserRoles());
+
+        }catch (AuthenticationException ex){
+            throw new CustomException("Used Username", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public String signUp(User user){
+        boolean isExisted = userRepository.existsByUsername(user.getUsername());
+
+        if(!isExisted){
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+            return jwtProvider.getToken(user.getUsername(),user.getUserRoles());
+        }else {
+            throw new CustomException("Used Username",HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public void deleteUser(String username){
+        userRepository.deleteByUsername(username);
+    }
+
+    public User getUser(String username){
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        return optionalUser.orElseThrow(() -> {
+            throw new CustomException("Used Username",HttpStatus.NOT_FOUND);
+        });
+    }
+
+    public String refreshToken(String username){
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        User user = optionalUser.orElseThrow(() -> {
+            throw new CustomException("Username Doesn't Exist",HttpStatus.BAD_REQUEST);
+        });
+
+        return jwtProvider.getToken(user.getUsername(),user.getUserRoles());
+    }
 
 }
